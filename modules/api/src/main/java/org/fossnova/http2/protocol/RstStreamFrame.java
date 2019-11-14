52435a5ee31d814c3351e3f58e8341c0c5dd0444
Richard Opálka <opalka.richard@gmail.com>
2019-11-14 13:51:29 +0100

    Initial Rst Stream Frame API and tests

---
+++ modules/api/src/main/java/org/fossnova/http2/protocol/RstStreamFrame.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2012-2019, FOSS Nova Software foundation (FNSF),
+ * and individual contributors as indicated by the @author tags.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.fossnova.http2.protocol;
+
+/**
+ * // TODO: javadoc
+ * @author <a href="mailto:opalka.richard@gmail.com">Richard Opalka</a>
+ */
+public interface RstStreamFrame extends Frame {
+    int getErrorCode();
+
+    static Builder newBuilder() {
+        // TODO: implement
+        throw new UnsupportedOperationException();
+    }
+
+    interface Builder extends Frame.Builder {
+        void setErrorCode(int weight);
+        RstStreamFrame build();
+    }
+}
---
+++ modules/tests/src/test/java/test/fossnova/http2/stream/RstStreamFrameRoundTripTestCase.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2012-2019, FOSS Nova Software foundation (FNSF),
+ * and individual contributors as indicated by the @author tags.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package test.fossnova.http2.stream;
+
+import org.fossnova.http2.protocol.RstStreamFrame;
+import org.junit.Test;
+
+import static org.fossnova.http2.protocol.RstStreamFrame.Builder;
+import static org.fossnova.http2.protocol.RstStreamFrame.newBuilder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author <a href="mailto:opalka.richard@gmail.com">Richard Opalka</a>
+ */
+public class RstStreamFrameRoundTripTestCase extends AbstractHttp2TestCase {
+    @Test
+    public void rstStreamFrame() {
+        writeRstStreamFrame();
+        readRstStreamFrame();
+    }
+
+    private void writeRstStreamFrame() {
+        RstStreamFrame.Builder builder = newBuilder();
+        builder.setPayloadSize(4); // if not invoked defaults to 0
+        builder.setErrorCode(500);
+        framesHandler.push(builder.build());
+    }
+
+    private void readRstStreamFrame() {
+        RstStreamFrame frame = (RstStreamFrame) framesHandler.pull();
+        assertNotNull(frame);
+        assertEquals(frame.getSize(), 4);
+        assertEquals(frame.getErrorCode(), 500);
+    }
+}
