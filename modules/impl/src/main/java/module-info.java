71944868069541eecc652d10c53c096e7a852adf
Richard Opálka <opalka.richard@gmail.com>
2019-12-10 19:43:52 +0100

    JPMS-ize maven modules

---
+++ modules/impl/src/main/java/module-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2012-2019, FOSS Nova Software foundation (FNSF),
+ * and individual contributors as indicated by the @author tags.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+/**
+ * @author <a href="mailto:opalka.richard@gmail.com">Richard Opalka</a>
+ */
+module com.fossnova.http2.impl {
+    requires org.fossnova.http2.api;
+    exports com.fossnova.http2.protocol to org.fossnova.http2.protocol;
+}
