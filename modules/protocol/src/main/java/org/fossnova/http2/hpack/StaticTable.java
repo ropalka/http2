d0de57cff0d062fd3b800d45f80c28b1aec5879a
Richard Opálka <opalka.richard@gmail.com>
2020-04-03 13:58:08 +0200

    Introducing StaticTable class

---
+++ modules/protocol/src/main/java/org/fossnova/http2/hpack/StaticTable.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2012-2020, FOSS Nova Software foundation (FNSF),
+ * and individual contributors as indicated by the @author tags.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.fossnova.http2.hpack;
+
+import static org.fossnova.http2.Header.*;
+import static org.fossnova.http2.Method.*;
+import static org.fossnova.http2.Scheme.*;
+import static org.fossnova.http2.StatusCode.*;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author <a href="mailto:opalka.richard@gmail.com">Richard Opalka</a>
+ */
+final class StaticTable {
+
+    private static final Charset CHARSET = StandardCharsets.UTF_8;
+    private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
+    private static final byte[][] HEADER_NAMES = new byte[62][];
+    private static final byte[][] HEADER_VALUES = new byte[62][];
+
+    static {
+        int i = 1;
+        HEADER_NAMES[i++] = AUTHORITY.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = METHOD.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = METHOD.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = PATH.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = PATH.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = SCHEME.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = SCHEME.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = ACCEPT_CHARSET.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = ACCEPT_ENCODING.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = ACCEPT_LANGUAGE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = ACCEPT_RANGES.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = ACCEPT.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = ACCESS_CONTROL_ALLOW_ORIGIN.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = AGE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = ALLOW.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = AUTHORIZATION.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = CACHE_CONTROL.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = CONTENT_DISPOSITION.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = CONTENT_ENCODING.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = CONTENT_LANGUAGE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = CONTENT_LENGTH.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = CONTENT_LOCATION.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = CONTENT_RANGE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = CONTENT_TYPE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = COOKIE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = DATE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = ETAG.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = EXPECT.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = EXPIRES.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = FROM.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = HOST.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = IF_MATCH.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = IF_MODIFIED_SINCE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = IF_NONE_MATCH.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = IF_RANGE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = IF_UNMODIFIED_SINCE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = LAST_MODIFIED.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = LINK.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = LOCATION.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = MAX_FORWARDS.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = PROXY_AUTHENTICATE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = PROXY_AUTHORIZATION.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = RANGE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = REFERER.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = REFRESH.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = RETRY_AFTER.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = SERVER.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = SET_COOKIE.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = STRICT_TRANSPORT_SECURITY.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = TRANSFER_ENCODING.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = USER_AGENT.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = VARY.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = VIA.getLowerCaseName().getBytes(CHARSET);
+        HEADER_NAMES[i++] = WWW_AUTHENTICATE.getLowerCaseName().getBytes(CHARSET);
+        i = 1;
+        HEADER_VALUES[i++] = EMPTY_BYTE_ARRAY;
+        HEADER_VALUES[i++] = GET.getName().getBytes(CHARSET);
+        HEADER_VALUES[i++] = POST.getName().getBytes(CHARSET);
+        HEADER_VALUES[i++] = "/".getBytes(CHARSET);
+        HEADER_VALUES[i++] = "/index.html".getBytes(CHARSET);
+        HEADER_VALUES[i++] = HTTP.getName().getBytes(CHARSET);
+        HEADER_VALUES[i++] = HTTPS.getName().getBytes(CHARSET);
+        HEADER_VALUES[i++] = OK.getCodeAsString().getBytes(CHARSET);
+        HEADER_VALUES[i++] = NO_CONTENT.getCodeAsString().getBytes(CHARSET);
+        HEADER_VALUES[i++] = PARTIAL_CONTENT.getCodeAsString().getBytes(CHARSET);
+        HEADER_VALUES[i++] = NOT_MODIFIED.getCodeAsString().getBytes(CHARSET);
+        HEADER_VALUES[i++] = BAD_REQUEST.getCodeAsString().getBytes(CHARSET);
+        HEADER_VALUES[i++] = NOT_FOUND.getCodeAsString().getBytes(CHARSET);
+        HEADER_VALUES[i++] = INTERNAL_SERVER_ERROR.getCodeAsString().getBytes(CHARSET);
+        HEADER_VALUES[i++] = EMPTY_BYTE_ARRAY;
+        HEADER_VALUES[i++] = "gzip, deflate".getBytes(CHARSET);
+        while (i < HEADER_VALUES.length) HEADER_VALUES[i++] = EMPTY_BYTE_ARRAY;
+    }
+
+    static int getSize() {
+        return HEADER_NAMES.length;
+    }
+
+    static int getStartIndex() {
+        return 1;
+    }
+
+}
