57a622b1bfc07ca505f7dd5eba71f36d2c22efa9
Richard Opálka <opalka.richard@gmail.com>
2020-04-03 17:18:05 +0200

    Refactoring StaticTable

--- modules/protocol/src/main/java/org/fossnova/http2/hpack/StaticTable.java
+++ modules/protocol/src/main/java/org/fossnova/http2/hpack/StaticTable.java
@@ -19,113 +19,134 @@
  */
 package org.fossnova.http2.hpack;
 
+import org.fossnova.http2.Header;
+import org.fossnova.http2.HeaderField;
+
 import static org.fossnova.http2.Header.*;
 import static org.fossnova.http2.Method.*;
 import static org.fossnova.http2.Scheme.*;
 import static org.fossnova.http2.StatusCode.*;
-
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 /**
  * @author <a href="mailto:opalka.richard@gmail.com">Richard Opalka</a>
  */
 final class StaticTable {
 
-    private static final Charset CHARSET = StandardCharsets.UTF_8;
-    private static final byte[] EMPTY_BYTE_ARRAY = new byte[0];
-    private static final byte[][] HEADER_NAMES = new byte[62][];
-    private static final byte[][] HEADER_VALUES = new byte[62][];
+    private static final HeaderField[] HEADER_FIELDS = new HeaderField[62];
+    private static final int ITEM_NOT_FOUND = -1;
 
     static {
         int i = 1;
-        HEADER_NAMES[i++] = AUTHORITY.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = METHOD.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = METHOD.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = PATH.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = PATH.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = SCHEME.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = SCHEME.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = STATUS.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = ACCEPT_CHARSET.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = ACCEPT_ENCODING.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = ACCEPT_LANGUAGE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = ACCEPT_RANGES.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = ACCEPT.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = ACCESS_CONTROL_ALLOW_ORIGIN.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = AGE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = ALLOW.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = AUTHORIZATION.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = CACHE_CONTROL.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = CONTENT_DISPOSITION.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = CONTENT_ENCODING.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = CONTENT_LANGUAGE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = CONTENT_LENGTH.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = CONTENT_LOCATION.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = CONTENT_RANGE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = CONTENT_TYPE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = COOKIE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = DATE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = ETAG.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = EXPECT.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = EXPIRES.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = FROM.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = HOST.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = IF_MATCH.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = IF_MODIFIED_SINCE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = IF_NONE_MATCH.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = IF_RANGE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = IF_UNMODIFIED_SINCE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = LAST_MODIFIED.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = LINK.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = LOCATION.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = MAX_FORWARDS.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = PROXY_AUTHENTICATE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = PROXY_AUTHORIZATION.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = RANGE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = REFERER.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = REFRESH.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = RETRY_AFTER.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = SERVER.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = SET_COOKIE.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = STRICT_TRANSPORT_SECURITY.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = TRANSFER_ENCODING.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = USER_AGENT.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = VARY.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = VIA.getLowerCaseName().getBytes(CHARSET);
-        HEADER_NAMES[i++] = WWW_AUTHENTICATE.getLowerCaseName().getBytes(CHARSET);
-        i = 1;
-        HEADER_VALUES[i++] = EMPTY_BYTE_ARRAY;
-        HEADER_VALUES[i++] = GET.getName().getBytes(CHARSET);
-        HEADER_VALUES[i++] = POST.getName().getBytes(CHARSET);
-        HEADER_VALUES[i++] = "/".getBytes(CHARSET);
-        HEADER_VALUES[i++] = "/index.html".getBytes(CHARSET);
-        HEADER_VALUES[i++] = HTTP.getName().getBytes(CHARSET);
-        HEADER_VALUES[i++] = HTTPS.getName().getBytes(CHARSET);
-        HEADER_VALUES[i++] = OK.getCodeAsString().getBytes(CHARSET);
-        HEADER_VALUES[i++] = NO_CONTENT.getCodeAsString().getBytes(CHARSET);
-        HEADER_VALUES[i++] = PARTIAL_CONTENT.getCodeAsString().getBytes(CHARSET);
-        HEADER_VALUES[i++] = NOT_MODIFIED.getCodeAsString().getBytes(CHARSET);
-        HEADER_VALUES[i++] = BAD_REQUEST.getCodeAsString().getBytes(CHARSET);
-        HEADER_VALUES[i++] = NOT_FOUND.getCodeAsString().getBytes(CHARSET);
-        HEADER_VALUES[i++] = INTERNAL_SERVER_ERROR.getCodeAsString().getBytes(CHARSET);
-        HEADER_VALUES[i++] = EMPTY_BYTE_ARRAY;
-        HEADER_VALUES[i++] = "gzip, deflate".getBytes(CHARSET);
-        while (i < HEADER_VALUES.length) HEADER_VALUES[i++] = EMPTY_BYTE_ARRAY;
+        HEADER_FIELDS[i++] = HeaderField.of(AUTHORITY);
+        HEADER_FIELDS[i++] = HeaderField.of(METHOD, GET.getName());
+        HEADER_FIELDS[i++] = HeaderField.of(METHOD, POST.getName());
+        HEADER_FIELDS[i++] = HeaderField.of(PATH, "/");
+        HEADER_FIELDS[i++] = HeaderField.of(PATH, "/index.html");
+        HEADER_FIELDS[i++] = HeaderField.of(SCHEME, HTTP.getName());
+        HEADER_FIELDS[i++] = HeaderField.of(SCHEME, HTTPS.getName());
+        HEADER_FIELDS[i++] = HeaderField.of(STATUS, OK.getCodeAsString());
+        HEADER_FIELDS[i++] = HeaderField.of(STATUS, NO_CONTENT.getCodeAsString());
+        HEADER_FIELDS[i++] = HeaderField.of(STATUS, PARTIAL_CONTENT.getCodeAsString());
+        HEADER_FIELDS[i++] = HeaderField.of(STATUS, NOT_MODIFIED.getCodeAsString());
+        HEADER_FIELDS[i++] = HeaderField.of(STATUS, BAD_REQUEST.getCodeAsString());
+        HEADER_FIELDS[i++] = HeaderField.of(STATUS, NOT_FOUND.getCodeAsString());
+        HEADER_FIELDS[i++] = HeaderField.of(STATUS, INTERNAL_SERVER_ERROR.getCodeAsString());
+        HEADER_FIELDS[i++] = HeaderField.of(ACCEPT_CHARSET);
+        HEADER_FIELDS[i++] = HeaderField.of(ACCEPT_ENCODING, "gzip, deflate");
+        HEADER_FIELDS[i++] = HeaderField.of(ACCEPT_LANGUAGE);
+        HEADER_FIELDS[i++] = HeaderField.of(ACCEPT_RANGES);
+        HEADER_FIELDS[i++] = HeaderField.of(ACCEPT);
+        HEADER_FIELDS[i++] = HeaderField.of(ACCESS_CONTROL_ALLOW_ORIGIN);
+        HEADER_FIELDS[i++] = HeaderField.of(AGE);
+        HEADER_FIELDS[i++] = HeaderField.of(ALLOW);
+        HEADER_FIELDS[i++] = HeaderField.of(AUTHORIZATION);
+        HEADER_FIELDS[i++] = HeaderField.of(CACHE_CONTROL);
+        HEADER_FIELDS[i++] = HeaderField.of(CONTENT_DISPOSITION);
+        HEADER_FIELDS[i++] = HeaderField.of(CONTENT_ENCODING);
+        HEADER_FIELDS[i++] = HeaderField.of(CONTENT_LANGUAGE);
+        HEADER_FIELDS[i++] = HeaderField.of(CONTENT_LENGTH);
+        HEADER_FIELDS[i++] = HeaderField.of(CONTENT_LOCATION);
+        HEADER_FIELDS[i++] = HeaderField.of(CONTENT_RANGE);
+        HEADER_FIELDS[i++] = HeaderField.of(CONTENT_TYPE);
+        HEADER_FIELDS[i++] = HeaderField.of(COOKIE);
+        HEADER_FIELDS[i++] = HeaderField.of(DATE);
+        HEADER_FIELDS[i++] = HeaderField.of(ETAG);
+        HEADER_FIELDS[i++] = HeaderField.of(EXPECT);
+        HEADER_FIELDS[i++] = HeaderField.of(EXPIRES);
+        HEADER_FIELDS[i++] = HeaderField.of(FROM);
+        HEADER_FIELDS[i++] = HeaderField.of(HOST);
+        HEADER_FIELDS[i++] = HeaderField.of(IF_MATCH);
+        HEADER_FIELDS[i++] = HeaderField.of(IF_MODIFIED_SINCE);
+        HEADER_FIELDS[i++] = HeaderField.of(IF_NONE_MATCH);
+        HEADER_FIELDS[i++] = HeaderField.of(IF_RANGE);
+        HEADER_FIELDS[i++] = HeaderField.of(IF_UNMODIFIED_SINCE);
+        HEADER_FIELDS[i++] = HeaderField.of(LAST_MODIFIED);
+        HEADER_FIELDS[i++] = HeaderField.of(LINK);
+        HEADER_FIELDS[i++] = HeaderField.of(LOCATION);
+        HEADER_FIELDS[i++] = HeaderField.of(MAX_FORWARDS);
+        HEADER_FIELDS[i++] = HeaderField.of(PROXY_AUTHENTICATE);
+        HEADER_FIELDS[i++] = HeaderField.of(PROXY_AUTHORIZATION);
+        HEADER_FIELDS[i++] = HeaderField.of(RANGE);
+        HEADER_FIELDS[i++] = HeaderField.of(REFERER);
+        HEADER_FIELDS[i++] = HeaderField.of(REFRESH);
+        HEADER_FIELDS[i++] = HeaderField.of(RETRY_AFTER);
+        HEADER_FIELDS[i++] = HeaderField.of(SERVER);
+        HEADER_FIELDS[i++] = HeaderField.of(SET_COOKIE);
+        HEADER_FIELDS[i++] = HeaderField.of(STRICT_TRANSPORT_SECURITY);
+        HEADER_FIELDS[i++] = HeaderField.of(TRANSFER_ENCODING);
+        HEADER_FIELDS[i++] = HeaderField.of(USER_AGENT);
+        HEADER_FIELDS[i++] = HeaderField.of(VARY);
+        HEADER_FIELDS[i++] = HeaderField.of(VIA);
+        HEADER_FIELDS[i++] = HeaderField.of(WWW_AUTHENTICATE);
     }
 
-    static int getSize() {
-        return HEADER_NAMES.length;
+    /**
+     * Returns index of header field with potentially empty header value inside static table.
+     * @param hf header field
+     * @return index of header field inside static table or <code>-1</code> if no such header field is present
+     * @throws NullPointerException if method parameter is null
+     */
+    static int indexOf(final HeaderField hf) {
+        if (hf == null) throw new NullPointerException();
+        for (int i = 1; i < HEADER_FIELDS.length; i++) {
+            if (hf.equals(HEADER_FIELDS[i])) return i;
+        }
+        return ITEM_NOT_FOUND;
     }
 
-    static int getStartIndex() {
-        return 1;
+    /**
+     * Returns index of header name inside static table.
+     * @param h message header
+     * @return index of header name inside static table or <code>-1</code> if none such header name is present
+     * @throws NullPointerException if method parameter is null
+     */
+    static int indexOf(final Header h) {
+        if (h == null) throw new NullPointerException();
+        for (int i = 1; i < HEADER_FIELDS.length; i++) {
+            if (HEADER_FIELDS[i].getHeaderName().equals(h)) return i;
+        }
+        return ITEM_NOT_FOUND;
+    }
+
+    /**
+     * Returns header field for given index.
+     * @param index inside static table
+     * @return header field for given index
+     * @throws IllegalArgumentException if index contains invalid value
+     */
+    static HeaderField getHeaderField(final int index) {
+        if (index <= 1 || index >= HEADER_FIELDS.length) throw new IllegalArgumentException();
+        return HEADER_FIELDS[index];
+    }
+
+    /**
+     * Returns header name for given index.
+     * @param index inside static table
+     * @return header name for given index
+     * @throws IllegalArgumentException if index contains invalid value
+     */
+    static Header getHeader(final int index) {
+        return getHeaderField(index).getHeaderName();
     }
 
 }
